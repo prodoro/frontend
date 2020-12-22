4b62949e540cc2b9d9175359d5514fbcdabeda05
Ken Mueller <kenmueller0@gmail.com>
2020-12-21 16:58:52 -0800

    fixed dropdown content

--- components/AuthDropdownContent.tsx
+++ components/AuthDropdownContent.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import cx from 'classnames'
 
 import CurrentUser from 'models/CurrentUser'
@@ -17,22 +16,6 @@ export interface AuthDropdownContentProps {
 const AuthDropdownContent = ({ currentUser, onClick }: AuthDropdownContentProps) => (
 	<>
 		<EditUserName className={styles.row} user={currentUser} />
-		{currentUser.data
-			? (
-				<Link href={`/u/${currentUser.data.slug}`}>
-					<a className={cx(styles.row, styles.action)} onClick={onClick}>
-						<FontAwesomeIcon icon={faUser} />
-						<span className={styles.actionMessage}>My files</span>
-					</a>
-				</Link>
-			)
-			: (
-				<p className={cx(styles.row, styles.action)} aria-disabled>
-					<FontAwesomeIcon icon={faUser} />
-					<span className={styles.actionMessage}>My files</span>
-				</p>
-			)
-		}
 		<SignOutButton className={cx(styles.row, styles.action, styles.danger)} onClick={onClick}>
 			<FontAwesomeIcon icon={faSignOutAlt} />
 			<p className={styles.actionMessage}>Sign out</p>
